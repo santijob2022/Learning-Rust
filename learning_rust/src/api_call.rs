7547cf2367b03e488826bf52b15cc92c1b4c38c3
santijob2022 <santi.job.2022@gmail.com>
2025-06-08 20:11:24 -0600

    Init message and Basic API call in rust

---
+++ learning_rust/src/api_call.rs
@@ -0,0 +1,28 @@
+
+async  fn my_async_call(url: &str) -> Result<serde_json::Value, reqwest::Error>{
+    let response: serde_json::Value = reqwest::get(url)
+    .await?
+    .json::<serde_json::Value>()
+    .await?;
+
+    Ok(response)
+}
+
+#[cfg(test)]
+mod tests{
+    use  super::*;
+
+    #[tokio::test]
+    async fn tests_calls_fn(){
+        let api_url: &str = "https://catfact.ninja/fact";
+        let my_res = my_async_call(api_url).await;
+        match  my_res {
+            Ok(r)=> {
+                dbg!(r);
+            },
+            Err(_) => {
+                panic!("Request failed");
+            }
+        }        
+    }
+}
