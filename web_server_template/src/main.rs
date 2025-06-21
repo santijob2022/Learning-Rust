d309497dfb910471ea12c333918614b328f0d66a
santijob2022 <santi.job.2022@gmail.com>
2025-06-20 20:09:22 -0600

    Starting web server with Rust Project

---
+++ web_server_template/src/main.rs
@@ -0,0 +1,15 @@
+use actix_cors::Cors;
+use actix_web::{ http::header, web, App, HttpServer, Responder, HttpResponse };
+use serde::{ Deserialize, Serialize };
+use reqwest::Client as HttpClient;
+use async_trait::async_trait;
+
+use std::sync::Mutex;
+use std::collections::HashMap;
+use std::fs;
+use std::io::Write;
+
+
+fn main() {
+    println!("Hello, world!");
+}
