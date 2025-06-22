723a460757c0769a8803c56f341c7fbc78dd1d4e
santijob2022 <santi.job.2022@gmail.com>
2025-06-21 22:23:28 -0600

    Defining a mock Database

--- web_server_template/src/main.rs
+++ web_server_template/src/main.rs
@@ -9,6 +9,55 @@ use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>    
+}
+
+impl Database {
+    fn new() -> Self {
+        Self {
+            tasks: HashMap::new(),
+            users: HashMap::new()
+        }
+    }
+
+    // CRUD DATA
+    fn insert(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    fn get(&self, id: &u64) -> Option<&Task> {
+        self.tasks.get(id)
+    }
+
+    fn get_all(&self) -> Vec<&Task> {
+        self.tasks.values().collect()
+    }
+
+    fn delete(&mut self, id: &u64) {
+        self.tasks.remove(id);
+    }
+
+    fn update(&mut self, task: Task){
+        self.tasks.insert(task.id, task);
+    }
+}
 
 fn main() {
     println!("Hello, world!");
